d0ac5bc8f016b6f79fced826f63a6e88c3251339
Leonardo Galante <leonardo.galante@itsincom.it>
2024-01-25 17:43:53 +0100

    inserimento controllo soluzione sudoku

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@ namespace Sudoku
 {
     internal class Program
     {
+        public bool controlloSudoku(int[,] sudoku, int[,] soluzione)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if(sudoku[i, j] != soluzione[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             int[,] sudoku = {
@@ -22,8 +34,18 @@ namespace Sudoku
                     { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
                     { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
             };
+            int[,] soluzione = {
+                    { 3, 2, 1, 7, 5, 4, 6, 9, 8 },
+                    { 6, 4, 8, 2, 9, 3, 1, 5, 7 },
+                    { 5, 7, 9, 8, 1, 6, 2, 3, 4 },
+                    { 7, 8, 2, 3, 4, 5, 9, 6, 1},
+                    { 9, 6, 5, 1, 8, 7, 4, 2, 3 },
+                    { 1, 3, 4, 9, 6, 2, 8, 7, 5 },
+                    { 8, 9, 3, 5, 2, 1, 7, 4, 6 },
+                    { 2, 5, 6, 4, 7, 8, 3, 1, 9 },
+                    { 4, 1, 7, 6, 3, 9, 5, 8, 2 }
+            };
             StampaSudoku(sudoku);
-
             Console.ReadKey();
         }
         public static void StampaSudoku(int[,] sudoku)
