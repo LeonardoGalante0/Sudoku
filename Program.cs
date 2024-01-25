1a746c4ca74594c93fda29d4a217b12200cfe540
Leonardo Galante <leonardo.galante@itsincom.it>
2024-01-25 17:05:15 +0100

    Inserita funzione che stampa il sudoku
    Funzione StampaSudoku()

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,31 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sobkhl ,vkj gkj.asdjh fgasyukjxfhcjxnfklò o uia4ejb qsVGHQe");
+            int[,] sudoku = {
+                    { 3, 2, 1, 7, 0, 4, 0, 0, 0 },
+                    { 6, 4, 0, 0, 9, 0, 0, 0, 7 },
+                    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 4, 5, 9, 0, 0 },
+                    { 0, 0, 5, 1, 8, 7, 4, 0, 0 },
+                    { 0, 0, 4, 9, 6, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                    { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
+                    { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
+            };
+            StampaSudoku(sudoku);
+
+            Console.ReadKey();
+        }
+        public static void StampaSudoku(int[,] sudoku)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(sudoku[i, j] + " ");
+                }
+            }
         }
     }
 }
