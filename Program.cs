f8a939c3566c945064a8a08b0446d1688e0e321f
LeonardoGalante <LeonardoGalante@LEONARDO>
2024-01-25 16:04:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sobkhl ,vkj gkj.asdjh fgasyukjxfhcjxnfklò o uia4ejb qsVGHQe");
+        }
+    }
+}
